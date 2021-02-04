47c87472c30946dd658b76e77b3e063d3627a5d2
Nadirah Jama <Nadirahjama@gmail.com>
2021-02-03 23:39:09 -0600

    starting on notetaker assignment

---
+++ server.js
@@ -0,0 +1,27 @@
+// Dependecies
+
+const express = require("express");
+const fs = require("fs");
+const path = require('path');
+
+// express app
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+// express app to handle data parsing
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname));
+
+// Data
+
+const noteListItems = [];
+
+//Require routes file
+require('./routes/routes')(app);
+
+// Setup listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});  
